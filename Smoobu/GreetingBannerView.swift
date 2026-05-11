dd6655c817e0df2af02573de30bddb8bc68b0e90
erkutbas1 <erkut.bas@smoobu.com>
2026-05-11 12:47:01 +0300

    [SMOOBU-DEMO-10] Add a simple greeting banner view
    Automatically generated by Smoobu Dev Pipeline

---
+++ Smoobu/GreetingBannerView.swift
@@ -0,0 +1,27 @@
+// GreetingBannerView.swift // Smoobu //
+
+import SwiftUI
+
+struct GreetingBannerView: View {
+    let userName: String
+
+    private enum BannerSpacing {
+        static let extraSmall: CGFloat = 4
+        static let small: CGFloat = 8
+        static let semiMedium: CGFloat = 12
+        static let medium: CGFloat = 16
+        static let large: CGFloat = 24
+        static let extraLarge: CGFloat = 32
+    }
+
+    var body: some View {
+        Text("greeting_banner_welcome \(userName)")
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, BannerSpacing.medium)
+            .padding(.vertical, BannerSpacing.small)
+    }
+}
+
+#Preview {
+    GreetingBannerView(userName: "Alex")
+}
